889e259c8e036e4a0b9a4770712e6d2627a16859
David Li <dli86@jh.edu>
2023-03-01 20:14:17 -0500

    Add check for no data

--- module6-solution/app.js
+++ module6-solution/app.js
@@ -14,13 +14,13 @@
     }
 
     $scope.checkIfTooMuch = function() {
-      var items = $scope.items.split(",");
-      var count = 0;
-
       if (!$scope.items) {
         $scope.message = "Please enter data first";
         return;
       }
+
+      var items = $scope.items.split(",");
+      var count = 0;
 
       for (var i = 0; i < items.length; i++) {
         if (isEmpty(items[i])) {
