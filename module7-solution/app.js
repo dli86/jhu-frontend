293197791bd322b24ec72e32bba1d667e07b1811
David Li <dli86@jh.edu>
2023-03-19 12:05:47 -0400

    Add module 7 initial code

---
+++ module7-solution/app.js
@@ -0,0 +1,19 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController);
+
+ToBuyController.$inject = [];
+function ToBuyController() {
+
+}
+
+AlreadyBoughtController.$inject = [];
+function AlreadyBoughtController() {
+
+}
+
+})();
+  
