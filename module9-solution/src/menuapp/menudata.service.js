be0f2ac49a1184fd9f0c61b1920fc8c6506b7225
David Li <dli86@jh.edu>
2023-04-22 10:27:20 -0400

    Minor tests

--- module9-solution/src/menuapp/menudata.service.js
+++ module9-solution/src/menuapp/menudata.service.js
@@ -13,17 +13,17 @@
   
     // Pre-populate a no cookie list
     items.push({
-      name: "Sugar",
+      name: "Lunch",
       quantity: "2 bags",
       description: "Sugar used for baking delicious umm... baked goods."
     });
     items.push({
-      name: "flour",
+      name: "Dinner",
       quantity: "1 bags",
       description: "High quality wheat flour. Mix it with water, sugar, 2 raw eggs."
     });
     items.push({
-      name: "Chocolate Chips",
+      name: "Specials",
       quantity: "3 bags",
       description: "Put these in the dough. No reason, really. Gotta store them somewhere!"
     });
