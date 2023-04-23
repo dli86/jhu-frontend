16fe326394d78a3d9a667dec46ac37ca4ed1348e
David Li <dli86@jh.edu>
2023-04-23 11:11:38 -0400

    Add API calls

--- module9-solution/src/menuapp/menudata.service.js
+++ module9-solution/src/menuapp/menudata.service.js
@@ -2,69 +2,42 @@
   'use strict';
   
   angular.module('data')
-  .service('MenuDataService', MenuDataService);
+  .service('MenuDataService', MenuDataService)
+  .constant('ApiBasePath', "https://coursera-jhu-default-rtdb.firebaseio.com/");
   
-  MenuDataService.$inject = ['$q', '$timeout']
-  function MenuDataService($q, $timeout) {
+  MenuDataService.$inject = ['$http', 'ApiBasePath'];
+  function MenuDataService($http, ApiBasePath) {
     var service = this;
-  
-    // List of shopping items
-    var items = [];
-  
-    // Pre-populate a no cookie list
-    items.push({
-      name: "Lunch",
-      quantity: "2 bags",
-      description: "Sugar used for baking delicious umm... baked goods."
-    });
-    items.push({
-      name: "Dinner",
-      quantity: "1 bags",
-      description: "High quality wheat flour. Mix it with water, sugar, 2 raw eggs."
-    });
-    items.push({
-      name: "Specials",
-      quantity: "3 bags",
-      description: "Put these in the dough. No reason, really. Gotta store them somewhere!"
-    });
-  
-    // Simulates call to server
-    // Returns a promise, NOT items array directly
-    service.getItems = function () {
-      var deferred = $q.defer();
-  
-      // Wait 2 seconds before returning
-      $timeout(function () {
-        // deferred.reject(items);
-        deferred.resolve(items);
-      }, 800);
-  
-      return deferred.promise;
-    };
 
     service.getAllCategories = function() {
-      var deferred = $q.defer();
-  
-      // Wait 2 seconds before returning
-      $timeout(function () {
-        // deferred.reject(items);
-        deferred.resolve(items);
-      }, 800);
-  
-      return deferred.promise;
+
+      return $http({
+        url: (ApiBasePath + "categories.json")
+      })
+      .then(
+        function success(result) {
+          return result["data"];
+        },
+        function error(result) {
+          console.log("Error: ", result);
+        }
+      );
+
     };
 
     service.getItemsForCategory = function(categoryShortName) {
-      var deferred = $q.defer();
-      console.log("categoryShortName", categoryShortName);
-  
-      // Wait 2 seconds before returning
-      $timeout(function () {
-        // deferred.reject(items);
-        deferred.resolve(items);
-      }, 800);
-  
-      return deferred.promise;
+
+      return $http({
+        url: (ApiBasePath + "menu_items/" + categoryShortName + ".json")
+      })
+      .then(
+        function success(result) {
+          return result["data"]["menu_items"];
+        },
+        function error(result) {
+          console.log("Error: ", result);
+        }
+      );
     };
 
   }
